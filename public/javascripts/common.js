c9aaf726b17ff2c66556b7ac2e4d55ebebeebef0
cashely.shi <290119516@qq.com>
2017-05-25 13:47:34 +0800

    初始化项目

---
+++ public/javascripts/common.js
@@ -0,0 +1,20 @@
+export function browserRedirect(){
+    var iScale = 1;
+    iScale = iScale / window.devicePixelRatio;
+    document.write('<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=' + iScale + ',minimum-scale=' + iScale + ',maximum-scale=' + iScale + '">');
+    var iWidth = document.documentElement.clientWidth;
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+    if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+        document.getElementsByTagName('html')[0].style.fontSize = (iWidth / 750)*10 + 'px';
+    } else {
+        document.getElementsByTagName('html')[0].style.fontSize = 20 + 'px';
+    }
+}
